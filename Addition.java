e291a65f4dd77a8d5a14b495923c852812ae1d16
maananto <musfique2002.personal@gmail.com>
2024-09-24 21:42:01 +0600

    initial commit

--- Addition.java
+++ Addition.java
@@ -1,1 +1,13 @@
-
+package JavaFundamentals;
+import java.util.Scanner;
+public class Addition {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int value1,value2,sum;
+        System.out.println("Enter two values for addition:");
+        value1=input.nextInt();
+        value2=input.nextInt();
+        sum=value1+value2;
+        System.out.println("The Sum of "+value1+" & "+value2+" is = "+sum);
+    }
+}
