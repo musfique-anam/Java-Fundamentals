e348d45471ed0fe8cd84b932a32a3e82b4a9634d
maananto <musfique2002.personal@gmail.com>
2024-09-25 20:49:16 +0600

    initial commit

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+package JavaFundamentals;
+import java.util.Scanner;
+public class Multiplication {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int value1,value2,product;
+        System.out.println("Enter two values:");
+        value1=scan.nextInt();
+        value2=scan.nextInt();
+        product=value1*value2;
+        System.out.println("The product of "+value1+" & "+value2+" is = "+product);
+    }
+}
