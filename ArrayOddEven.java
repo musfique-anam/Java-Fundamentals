7b85a62fdaf8d4cb43462a83585458e4ef48263f
maananto <musfique2002.personal@gmail.com>
2024-09-27 14:45:14 +0600

    Odd-Even Counter

---
+++ ArrayOddEven.java
@@ -0,0 +1,26 @@
+package JavaFundamentals;
+import java.util.Scanner;
+public class ArrayOddEven {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the total number of elements:");
+        int n=scan.nextInt();
+        int array[]=new int[n];
+        System.out.println("Enter the elements:");
+        for(int i=0;i<array.length;i++)
+            array[i]=scan.nextInt();
+        countOddEven(array);
+    }
+    public static void countOddEven(int array[]){
+        int countOdd=0;
+        int countEven=0;
+        for(int i=0;i<array.length;i++)
+            if(array[i] % 2== 0)
+                ++countEven;
+            else
+                ++countOdd;
+        System.out.println("Count of total odd numbers are :"+countOdd);
+        System.out.println("Count of total even numbers are :"+countEven);
+    }
+    
+}
