ff03341ff0547c87c59d79687736899c66218ba0
maananto <musfique2002.personal@gmail.com>
2024-09-29 16:08:23 +0600

    Prime Number detector

---
+++ PrimeNumber.java
@@ -0,0 +1,19 @@
+package JavaFundamentals;
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int num =scan.nextInt();
+        boolean flag=false;
+        for(int i=2;i<=num/2;++i){
+            if(num%i==0){
+                flag=true;
+                break;
+            }
+        }if(!flag)
+            System.out.println(num+" is a Prime Number.");
+        else
+            System.out.println(num+" is not a Prime Number.");
+    }    
+}
