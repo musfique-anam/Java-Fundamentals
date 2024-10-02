4e114a533836f60a6f9553fe84faa4d5807ac36a
maananto <musfique2002.personal@gmail.com>
2024-10-02 15:39:17 +0600

    Printing a pyramid

--- PyramidPattern.java
+++ PyramidPattern.java
@@ -5,12 +5,14 @@ public class PyramidPattern {
         Scanner reader=new Scanner(System.in);
         System.out.println("Enter the number of rows you want to watch : ");
         int rows=reader.nextInt();
-        for(int i = 1;i<=rows-i;++j){
-            System.out.print(" ");
+        for(int i = 1;i<=rows;++i){
+            for(int j=1;j<=rows-i;++j){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;++k){
+                System.out.print("* ");
+            }
+            System.out.println();
         }
-        for(int k=1;k<= i;++k){
-            System.out.print(" * ");
-        }
-        System.out.println();
     }
 }
